1486dbfac0e834f44059a32dff51848a5057d7b9
HAZM <muhammadnurs8191@gmail.com>
2026-08-23 09:36:25 -0600

    Update colors navigation

--- public/app.js
+++ public/app.js
@@ -85,7 +85,7 @@ function storefrontProducts() {
 }
 
 function allColorProducts() {
-  return PRODUCTS.filter((product) => product.id !== 'testing' && !METALLIC_PRODUCT_IDS.has(product.id));
+  return PRODUCTS.filter((product) => product.id !== 'testing');
 }
 
 function productImage(product) {
